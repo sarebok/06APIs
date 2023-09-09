a04a655c2db56c04fbbebdb37c2c040c11238ec3
sarebok <bortonae@hotmail.com>
2023-09-09 11:39:30 -0300

    finish graph functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 let data = [];
 let dolarObject = [];
 let euroObject = [];
+let currencyObject = [];
 let getPesos = 0,
   getDolar = 0,
   getEuro = 0;
@@ -9,25 +10,24 @@ const pesosInput = document.getElementById("input-clp");
 const convertBtn = document.getElementById("convert-currency-btn");
 const conversion = document.getElementById("conversion");
 const selectCurrency = document.getElementById("select-currency");
+const chartExt = document.querySelector("#chart");
 
 const convert = (monedaOriginal, convertirACurrency) => {
   const conversion = monedaOriginal / convertirACurrency;
   return conversion;
 };
 
-const convertOnClick = () => {
-  convertBtn.addEventListener("click", () => {
-    getPesos = pesosInput.value;
-    getDolar = dolarObject.valor;
-    getEuro = euroObject.valor;
-    if (selectCurrency.value == "USD") {
-      conversion.innerHTML = convert(getPesos, getDolar).toFixed(2);
-    } else if (selectCurrency.value == "EUR") {
-      conversion.innerHTML = convert(getPesos, getEuro).toFixed(2);
-    }
+const indicatorOnClick = () => {
+  getPesos = pesosInput.value;
+  getDolar = dolarObject.valor;
+  getEuro = euroObject.valor;
+  if (selectCurrency.value == "dolar") {
+    conversion.innerHTML = convert(getPesos, getDolar).toFixed(2);
+  } else if (selectCurrency.value == "euro") {
+    conversion.innerHTML = convert(getPesos, getEuro).toFixed(2);
+  }
 
-    //conversion.innerHTML = convert(getPesos, getDolar);
-  });
+  //conversion.innerHTML = convert(getPesos, getDolar);
 };
 
 const getMiIndicador = async () => {
@@ -41,13 +41,110 @@ const getHistoricCurrency = async (currency) => {
   const resp = await fetch(`https://mindicador.cl/api/${currency}`);
   const json = await resp.json();
   currencyObject = json;
-  console.log(currencyObject);
+  console.log("moneda es: " + currency);
 };
 
-const init = async () => {
+//comienza el grafico
+const lineChart = (series, categories) => {
+  const options = {
+    series: [
+      {
+        name: "Desktops",
+        data: series,
+      },
+    ],
+    chart: {
+      height: 350,
+      type: "line",
+      zoom: {
+        enabled: false,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "straight",
+    },
+    title: {
+      text: "Product Trends by Month",
+      align: "left",
+    },
+    grid: {
+      row: {
+        colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+        opacity: 0.5,
+      },
+    },
+    xaxis: {
+      categories: categories,
+    },
+  };
+  chartExt.innerHTML = "";
+
+  const chart = new ApexCharts(chartExt, options);
+  chart.render();
+};
+
+const init = async (tipoCambio) => {
   await getMiIndicador();
-  await getHistoricCurrency("dolar");
-  convertOnClick();
+  await getHistoricCurrency(tipoCambio);
+  indicatorOnClick();
+  console.log(currencyObject);
+  let fechaCurrency = [];
+  let valorCurrency = [];
+  for (let i = 0; i < 10; i++) {
+    fechaCurrency.push(currencyObject.serie[i].fecha);
+    valorCurrency.push(currencyObject.serie[i].valor);
+  }
+  console.log(fechaCurrency);
+  console.log(valorCurrency);
+  lineChart(valorCurrency, fechaCurrency);
+  //for (object of currencyObject.serie) console.log(object.fecha);
+
+  //  lineChart(valorCurrency, fechaCurrency);
 };
 
-init();
+let currencySelected = "";
+convertBtn.addEventListener("click", () => {
+  currencySelected = selectCurrency.value;
+  init(currencySelected);
+});
+
+/* const options = {
+  series: [
+    {
+      name: "Desktops",
+      data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+    },
+  ],
+  chart: {
+    height: 350,
+    type: "line",
+    zoom: {
+      enabled: false,
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: "straight",
+  },
+  title: {
+    text: "Product Trends by Month",
+    align: "left",
+  },
+  grid: {
+    row: {
+      colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+      opacity: 0.5,
+    },
+  },
+  xaxis: {
+    categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"],
+  },
+}; */
+
+//const chart = new ApexCharts(document.querySelector("#chart"), options);
+//chart.render();
